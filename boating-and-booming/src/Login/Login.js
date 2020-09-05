2d1837861fe3d2313d0c736bb7405c3c60628a32
zhangyiloong <zhangyiloong@gmail.com>
2020-09-04 23:56:41 -0700

    adjust login ui

--- boating-and-booming/src/Login/Login.js
+++ boating-and-booming/src/Login/Login.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
-class Login extends React.Component {
-    render() {
-        return (
-            <div>
-                用户名:<input type='text'></input>
-                密码:<input type='password'></input>
-                <button onClick={() => {window.location.href = '#/gamemode'}}>登陆</button>
-            </div>
-        )
-    }
+const useStyles = makeStyles((theme) => ({
+  form: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+
+  buttom: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+
+}));
+
+export default function Login() {
+
+    const classes = useStyles();
+
+    return (
+      <div>
+        <form className={classes.form} id="login-form">
+          <TextField required id="name" label="用户名" variant="outlined" />
+          <TextField required id="password" label="密码" type="password" autoComplete="current-password" variant="outlined" />
+        </form>
+        <div>
+          <Button variant="contained" color="primary" onClick={() => {window.location.href = '#/gamemode'}}>登陆</Button>
+          <Button variant="contained" onClick={() => {document.getElementById("login-form").reset()}}>取消</Button>
+        </div>
+      </div>
+    );
 }
 
-export default Login;
